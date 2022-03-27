5c1e755c8c2c68904f9f5a5bd5cad098b575ef02
pofa2007 <pofa2007@users.noreply.github.com>
2022-03-27 19:57:37 +0100

    Update run0.sh

--- run0.sh
+++ run0.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+
+#put this on crontab every week, no need to do it dayly
+
 cd /home/administrador1/mesh_geoip
 
+#get updated geoip database and save it to countrycode.json.txt
 python3 maxmind_get.py
 
 systemctl stop meshcentral8843
 systemctl status meshcentral8843
+
+#update meshcentral config
 python3 meshcentral_update.py /home/meshcentral/meshcentral-data/config.json ./2264397.json.txt
+
 systemctl start meshcentral8843
 systemctl status meshcentral8843
 
